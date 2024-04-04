c006921f9ebb78d6f3ba5e47b2ea96b79924b99b
Noah Tilleul <noah.tilleul@etu.unilim.fr>
2024-04-04 09:40:06 +0200

    ajout ScrabbleApplicationConsole

---
+++ src/main/java/scrabble/application/ScrabbleApplicationConsole.java
@@ -0,0 +1,11 @@
+package scrabble.application;
+
+public class ScrabbleApplicationConsole {
+
+	public static void main(String[] args) {
+		System.out.println("-----------------------------------------------------");
+		System.out.println("-- Binevenue dans notre magnifique jeu de scrabble --");
+		System.out.println("-----------------------------------------------------");
+	}
+
+}

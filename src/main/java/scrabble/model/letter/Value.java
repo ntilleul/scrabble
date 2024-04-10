8bb2caba806393abcc18f326d78f32bcff1ae328
Andréas REY MALISSEIN <andreas.reymalissein@etu.unilim.fr>
2024-04-10 12:41:46 +0200

    ajout point lettre et test points

--- src/main/java/scrabble/model/letter/Value.java
+++ src/main/java/scrabble/model/letter/Value.java
@@ -2,15 +2,24 @@ package scrabble.model.letter;
 
 public enum Value {
 
-    A('A'), B('B'), C('C'), D('D'), E('E'), F('F'),G('G'),H('H'),I('I'),J('J'),K('K'),L('L'),M('M'),N('N'),O('O'),P('P'),Q('Q'),R('R'),S('S'),T('T'),U('U'),V('V'),W('W'),X('X'),Y('Y'),Z('Z'), JOKER(' ');
+    A('A', 1), B('B', 3), C('C', 3), D('D', 2), E('E', 1), F('F', 4), G('G', 2), H('H', 4),
+    I('I', 1), J('J', 8), K('K', 5), L('L', 1), M('M', 3), N('N', 1), O('O', 1), P('P', 3),
+    Q('Q', 10), R('R', 1), S('S', 1), T('T', 1), U('U', 1), V('V', 4), W('W', 4), X('X', 8),
+    Y('Y', 4), Z('Z', 10), JOKER(' ', 0);
 
-    private final char code;
+    private final char value;
+    private final int points;
 
-    private Value(char code) {
-        this.code= code;
+    private Value(char value, int points) {
+
+        this.value= value;
+        this.points = points;
     }
-    public char getCode() {
-        return this.code;
+    public char getValue() {
+        return this.value;
     }
 
+    public int getPoints() {
+        return this.points;
+    }
 }

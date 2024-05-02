377a5363d00b021f893fa6ea1b288edc3be12b68
Noah Tilleul <noah.tilleul@etu.unilim.fr>
2024-05-02 09:03:56 +0200

    print() board optimisation

--- src/main/java/scrabble/model/game/Board.java
+++ src/main/java/scrabble/model/game/Board.java
@@ -6,34 +6,16 @@ public class Board {
 	
 	public void print() {
 		System.out.println("┌─┬─┬─┬─┬─┬─┬─┬─┬─┬─┬─┬─┬─┬─┬─┐");
-		System.out.println("| | | | | | | | | | | | | | | |");
-		System.out.println("├─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┤");
-		System.out.println("| | | | | | | | | | | | | | | |");
-		System.out.println("├─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┤");
-		System.out.println("| | | | | | | | | | | | | | | |");
-		System.out.println("├─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┤");
-		System.out.println("| | | | | | | | | | | | | | | |");
-		System.out.println("├─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┤");
-		System.out.println("| | | | | | | | | | | | | | | |");
-		System.out.println("├─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┤");
-		System.out.println("| | | | | | | | | | | | | | | |");
-		System.out.println("├─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┤");
-		System.out.println("| | | | | | | | | | | | | | | |");
-		System.out.println("├─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┤");
-		System.out.println("| | | | | | | |*| | | | | | | |");
-		System.out.println("├─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┤");
-		System.out.println("| | | | | | | | | | | | | | | |");
-		System.out.println("├─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┤");
-		System.out.println("| | | | | | | | | | | | | | | |");
-		System.out.println("├─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┤");
-		System.out.println("| | | | | | | | | | | | | | | |");
-		System.out.println("├─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┤");
-		System.out.println("| | | | | | | | | | | | | | | |");
-		System.out.println("├─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┤");
-		System.out.println("| | | | | | | | | | | | | | | |");
-		System.out.println("├─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┤");
-		System.out.println("| | | | | | | | | | | | | | | |");
-		System.out.println("├─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┤");
+		for (int i=0;i<14;i++) {
+			if (i == 7) {
+				System.out.println("| | | | | | | |*| | | | | | | |");
+				System.out.println("├─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┤");
+			}
+			else {
+				System.out.println("| | | | | | | | | | | | | | | |");
+				System.out.println("├─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┤");
+			}
+		}
 		System.out.println("| | | | | | | | | | | | | | | |");
 		System.out.println("└─┴─┴─┴─┴─┴─┴─┴─┴─┴─┴─┴─┴─┴─┴─┘");
 	}

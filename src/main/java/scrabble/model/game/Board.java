ad67b7dd3de2184ede1376873e0c84ba5c9d478f
Noah Tilleul <noah.tilleul@etu.unilim.fr>
2024-05-13 17:13:41 +0200

    add Cases in board and add getCase

--- src/main/java/scrabble/model/game/Board.java
+++ src/main/java/scrabble/model/game/Board.java
@@ -1,22 +1,40 @@
 package scrabble.model.game;
 
 public class Board {
+	
+	public Case[][] board ;
+	
 	public Board() {
+		board = new Case[15][15];
+		for (int i=0; i<15; i++) {
+			for (int j=0; j<15; j++) {
+				board[i][j]=new Case(i, j, 1);
+			}
+		}
+	}
+	
+	public Case getCase(int i, int j) {
+		return this.board[i][j];
 	}
 	
 	public void print() {
-		System.out.println("┌─┬─┬─┬─┬─┬─┬─┬─┬─┬─┬─┬─┬─┬─┬─┐");
-		for (int i=0;i<14;i++) {
-			if (i == 7) {
-				System.out.println("| | | | | | | |*| | | | | | | |");
-				System.out.println("├─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┤");
+		System.out.println("  |A|B|C|D|E|F|G|H|I|J|K|L|M|N|O|");
+		for (int i=0; i<15;i++) {
+			if (i < 9) {
+				if (i != 8) {
+					System.out.println("  ├─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┤");
+					System.out.println("0"+(i+1)+"|"+this.board[i][0]+"|"+this.board[i][1]+"|"+this.board[i][2]+"|"+this.board[i][3]+"|"+this.board[i][4]+"|"+this.board[i][5]+"|"+this.board[i][6]+"|"+this.board[i][7]+"|"+this.board[i][8]+"|"+this.board[i][9]+"|"+this.board[i][10]+"|"+this.board[i][11]+"|"+this.board[i][12]+"|"+this.board[i][13]+"|"+this.board[i][14]+"|");
+				}
+				else {
+					System.out.println("  ├─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┤");
+					System.out.println("0"+(i+1)+"|"+this.board[i][0]+"|"+this.board[i][1]+"|"+this.board[i][2]+"|"+this.board[i][3]+"|"+this.board[i][4]+"|"+this.board[i][5]+"|"+this.board[i][6]+"|"+this.board[i][7]+"|"+this.board[i][8]+"|"+this.board[i][9]+"|"+this.board[i][10]+"|"+this.board[i][11]+"|"+this.board[i][12]+"|"+this.board[i][13]+"|"+this.board[i][14]+"|");
+				}
 			}
 			else {
-				System.out.println("| | | | | | | | | | | | | | | |");
-				System.out.println("├─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┤");
+				System.out.println("  ├─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┤");
+				System.out.println((i+1)+"|"+this.board[i][0]+"|"+this.board[i][1]+"|"+this.board[i][2]+"|"+this.board[i][3]+"|"+this.board[i][4]+"|"+this.board[i][5]+"|"+this.board[i][6]+"|"+this.board[i][7]+"|"+this.board[i][8]+"|"+this.board[i][9]+"|"+this.board[i][10]+"|"+this.board[i][11]+"|"+this.board[i][12]+"|"+this.board[i][13]+"|"+this.board[i][14]+"|");
 			}
 		}
-		System.out.println("| | | | | | | | | | | | | | | |");
-		System.out.println("└─┴─┴─┴─┴─┴─┴─┴─┴─┴─┴─┴─┴─┴─┴─┘");
+		System.out.println("  └─┴─┴─┴─┴─┴─┴─┴─┴─┴─┴─┴─┴─┴─┴─┘");
 	}
 }

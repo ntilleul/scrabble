7b165230ef72d345aa227bf8732c94f832032cf3
Noah Tilleul <noah.tilleul@etu.unilim.fr>
2024-05-14 10:06:44 +0200

    fixing board print() method

--- src/main/java/scrabble/model/game/Board.java
+++ src/main/java/scrabble/model/game/Board.java
@@ -21,20 +21,26 @@ public class Board {
 		System.out.println("  |A|B|C|D|E|F|G|H|I|J|K|L|M|N|O|");
 		for (int i=0; i<15;i++) {
 			if (i < 9) {
-				if (i != 8) {
-					System.out.println("  ├─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┤");
+				if (i != 7) {
+					System.out.println("──┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┤");
 					System.out.println("0"+(i+1)+"|"+this.board[i][0]+"|"+this.board[i][1]+"|"+this.board[i][2]+"|"+this.board[i][3]+"|"+this.board[i][4]+"|"+this.board[i][5]+"|"+this.board[i][6]+"|"+this.board[i][7]+"|"+this.board[i][8]+"|"+this.board[i][9]+"|"+this.board[i][10]+"|"+this.board[i][11]+"|"+this.board[i][12]+"|"+this.board[i][13]+"|"+this.board[i][14]+"|");
 				}
 				else {
-					System.out.println("  ├─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┤");
-					System.out.println("0"+(i+1)+"|"+this.board[i][0]+"|"+this.board[i][1]+"|"+this.board[i][2]+"|"+this.board[i][3]+"|"+this.board[i][4]+"|"+this.board[i][5]+"|"+this.board[i][6]+"|"+this.board[i][7]+"|"+this.board[i][8]+"|"+this.board[i][9]+"|"+this.board[i][10]+"|"+this.board[i][11]+"|"+this.board[i][12]+"|"+this.board[i][13]+"|"+this.board[i][14]+"|");
+					if (this.board[i][7].toString()==" ") {
+						System.out.println("──┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┤");
+						System.out.println("0"+(i+1)+"|"+this.board[i][0]+"|"+this.board[i][1]+"|"+this.board[i][2]+"|"+this.board[i][3]+"|"+this.board[i][4]+"|"+this.board[i][5]+"|"+this.board[i][6]+"|*|"+this.board[i][8]+"|"+this.board[i][9]+"|"+this.board[i][10]+"|"+this.board[i][11]+"|"+this.board[i][12]+"|"+this.board[i][13]+"|"+this.board[i][14]+"|");
+					}
+					else {
+						System.out.println("──┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┤");
+						System.out.println("0"+(i+1)+"|"+this.board[i][0]+"|"+this.board[i][1]+"|"+this.board[i][2]+"|"+this.board[i][3]+"|"+this.board[i][4]+"|"+this.board[i][5]+"|"+this.board[i][6]+"|"+this.board[i][7]+"|"+this.board[i][8]+"|"+this.board[i][9]+"|"+this.board[i][10]+"|"+this.board[i][11]+"|"+this.board[i][12]+"|"+this.board[i][13]+"|"+this.board[i][14]+"|");
+					}
 				}
 			}
 			else {
-				System.out.println("  ├─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┤");
+				System.out.println("──┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┼─┤");
 				System.out.println((i+1)+"|"+this.board[i][0]+"|"+this.board[i][1]+"|"+this.board[i][2]+"|"+this.board[i][3]+"|"+this.board[i][4]+"|"+this.board[i][5]+"|"+this.board[i][6]+"|"+this.board[i][7]+"|"+this.board[i][8]+"|"+this.board[i][9]+"|"+this.board[i][10]+"|"+this.board[i][11]+"|"+this.board[i][12]+"|"+this.board[i][13]+"|"+this.board[i][14]+"|");
 			}
 		}
-		System.out.println("  └─┴─┴─┴─┴─┴─┴─┴─┴─┴─┴─┴─┴─┴─┴─┘");
+		System.out.println("──┴─┴─┴─┴─┴─┴─┴─┴─┴─┴─┴─┴─┴─┴─┴─┘");
 	}
 }

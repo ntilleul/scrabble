b0cbcac755b5eff09f2f62a296200fe5528ba8f0
Noah Tilleul <noah.tilleul@etu.unilim.fr>
2024-05-22 16:08:25 +0200

    add a method that verifies the correct placement of the first word

--- src/main/java/scrabble/model/game/Board.java
+++ src/main/java/scrabble/model/game/Board.java
@@ -120,4 +120,16 @@ public class Board {
 		}
 		System.out.println("──┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┘");
 	}
+
+
+	public boolean firstWordIsOnStar(String word, int x, int y, char dir) {
+		int length = word.length()-1;
+		if ((x > 7) || (y > 7)) {
+			return false;
+		}
+		if (((dir == 'H') && (length +  y >= 7))||((dir == 'V') && (length +  x >= 7)) ) {
+			return true;
+		}
+		return false;
+	}
 }

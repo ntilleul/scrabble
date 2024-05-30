fef19fa1272b15e544df57afaaf76c4960a87631
Florian Bussiere <florian.bussiere@etu.unilim.fr>
2024-05-30 14:58:59 +0200

    Refactoring of the multipliers setup

--- src/main/java/scrabble/model/game/Board.java
+++ src/main/java/scrabble/model/game/Board.java
@@ -17,85 +17,51 @@ public class Board {
 			}
 		}
 
+		board[7][7].setMultiplier(Multiplier.STAR);
+
+		initializeMultipliers();
+
+		completeSymmetry(board);
+
+	}
+
+	private void initializeMultipliers() {
 		board[0][0].setMultiplier(Multiplier.WORD_3);
+		board[0][3].setMultiplier(Multiplier.LETTER_2);
 		board[0][7].setMultiplier(Multiplier.WORD_3);
-		board[0][14].setMultiplier(Multiplier.WORD_3);
-
-		board[7][0].setMultiplier(Multiplier.WORD_3);
-		board[7][7].setMultiplier(Multiplier.WORD_3);
-		board[7][14].setMultiplier(Multiplier.WORD_3);
-
-		board[14][0].setMultiplier(Multiplier.WORD_3);
-		board[14][7].setMultiplier(Multiplier.WORD_3);
-		board[14][14].setMultiplier(Multiplier.WORD_3);
-
 		board[1][1].setMultiplier(Multiplier.WORD_2);
 		board[2][2].setMultiplier(Multiplier.WORD_2);
 		board[3][3].setMultiplier(Multiplier.WORD_2);
 		board[4][4].setMultiplier(Multiplier.WORD_2);
-
-		board[1][13].setMultiplier(Multiplier.WORD_2);
-		board[2][12].setMultiplier(Multiplier.WORD_2);
-		board[3][11].setMultiplier(Multiplier.WORD_2);
-		board[4][10].setMultiplier(Multiplier.WORD_2);
-
-		board[13][1].setMultiplier(Multiplier.WORD_2);
-		board[12][2].setMultiplier(Multiplier.WORD_2);
-		board[11][3].setMultiplier(Multiplier.WORD_2);
-		board[10][4].setMultiplier(Multiplier.WORD_2);
-
-		board[13][13].setMultiplier(Multiplier.WORD_2);
-		board[12][12].setMultiplier(Multiplier.WORD_2);
-		board[11][11].setMultiplier(Multiplier.WORD_2);
-		board[10][10].setMultiplier(Multiplier.WORD_2);
-
-		board[0][3].setMultiplier(Multiplier.LETTER_2);
-		board[0][11].setMultiplier(Multiplier.LETTER_2);
-		board[14][3].setMultiplier(Multiplier.LETTER_2);
-		board[14][11].setMultiplier(Multiplier.LETTER_2);
-
-		board[3][0].setMultiplier(Multiplier.LETTER_2);
-		board[11][0].setMultiplier(Multiplier.LETTER_2);
-		board[3][14].setMultiplier(Multiplier.LETTER_2);
-		board[11][14].setMultiplier(Multiplier.LETTER_2);
-
-		board[7][3].setMultiplier(Multiplier.LETTER_2);
-		board[6][2].setMultiplier(Multiplier.LETTER_2);
-		board[8][2].setMultiplier(Multiplier.LETTER_2);
-
-		board[7][11].setMultiplier(Multiplier.LETTER_2);
-		board[6][12].setMultiplier(Multiplier.LETTER_2);
-		board[8][12].setMultiplier(Multiplier.LETTER_2);
-
-		board[3][7].setMultiplier(Multiplier.LETTER_2);
+		board[5][5].setMultiplier(Multiplier.LETTER_3);
+		board[6][6].setMultiplier(Multiplier.LETTER_2);
+		board[1][5].setMultiplier(Multiplier.LETTER_3);
 		board[2][6].setMultiplier(Multiplier.LETTER_2);
 		board[2][8].setMultiplier(Multiplier.LETTER_2);
-
-		board[11][7].setMultiplier(Multiplier.LETTER_2);
-		board[12][6].setMultiplier(Multiplier.LETTER_2);
-		board[12][8].setMultiplier(Multiplier.LETTER_2);
-
-		board[6][6].setMultiplier(Multiplier.LETTER_2);
-		board[6][8].setMultiplier(Multiplier.LETTER_2);
-		board[8][6].setMultiplier(Multiplier.LETTER_2);
-		board[8][8].setMultiplier(Multiplier.LETTER_2);
-
-		board[5][5].setMultiplier(Multiplier.LETTER_3);
-		board[5][9].setMultiplier(Multiplier.LETTER_3);
-		board[9][5].setMultiplier(Multiplier.LETTER_3);
-		board[9][9].setMultiplier(Multiplier.LETTER_3);
-
-		board[1][5].setMultiplier(Multiplier.LETTER_3);
-		board[1][9].setMultiplier(Multiplier.LETTER_3);
-		board[13][5].setMultiplier(Multiplier.LETTER_3);
-		board[13][9].setMultiplier(Multiplier.LETTER_3);
-
+		board[3][0].setMultiplier(Multiplier.LETTER_2);
 		board[5][1].setMultiplier(Multiplier.LETTER_3);
-		board[9][1].setMultiplier(Multiplier.LETTER_3);
-		board[5][13].setMultiplier(Multiplier.LETTER_3);
-		board[9][13].setMultiplier(Multiplier.LETTER_3);
+		board[7][3].setMultiplier(Multiplier.LETTER_2);
+		board[6][2].setMultiplier(Multiplier.LETTER_2);
+		board[7][0].setMultiplier(Multiplier.WORD_3);
+		board[3][7].setMultiplier(Multiplier.LETTER_2);
+	}
 
-		board[7][7].setMultiplier(Multiplier.STAR);
+	private static void completeSymmetry(Tile[][] board) {
+		int size = board.length;
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				Multiplier multiplier = board[i][j].getMultiplier();
+				if (multiplier != null && multiplier != Multiplier.DEFAULT) {
+					board[j][i].setMultiplier(multiplier);
+					board[size - 1 - i][j].setMultiplier(multiplier);
+					board[j][size - 1 - i].setMultiplier(multiplier);
+					board[i][size - 1 - j].setMultiplier(multiplier);
+					board[size - 1 - j][i].setMultiplier(multiplier);
+					board[size - 1 - i][size - 1 - j].setMultiplier(multiplier);
+					board[size - 1 - j][size - 1 - i].setMultiplier(multiplier);
+				}
+			}
+		}
 	}
 
 	public Tile getTile(int i, int j) {
